12a3c8561b964706d7bef24b0facf2875b4bae22
facundoangel <facundonicolasangel@gmail.com>
2023-06-08 01:53:25 -0300

    terminado de test de ventas

--- test/programa/VentaTest.java
+++ test/programa/VentaTest.java
@@ -1,53 +1,118 @@
 package programa;
 
-import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
+import java.util.ArrayList;
+
 
 public class VentaTest {
 	/*
-	 * En este test se testea que ante cada accion de venta, afecte correctamente
-	 * a cada objeto que inteviene. Es decir a cada usuario le reste dinero y el tiempo
-	 * y a cada atraccion le reste el cupo correctamente
-	 * */
+	 * En este test se testea que ante cada accion de venta, afecte correctamente a
+	 * cada objeto que inteviene. Es decir a cada usuario le reste dinero y el
+	 * tiempo y a cada atraccion le reste el cupo correctamente
+	 */
+	
 
-	
-	
-	@Before
-	public void setUpBeforeClass() throws Exception {
-		Archivo arc = new Archivo("archivos-test/venta");
+
+
+	@Test
+	public void limiteVentas() {
+		/*
+		 * Se testea que la atraccion reduzca el cupo cuando se vende
+		 * y además que deje de estar disponible
+		 * */
+		boolean esperado1 = false;
+		boolean esperado2 = true;
+		
+		Atraccion atrac1 = new Atraccion("museo",100,1,1,"Paisaje");
+		Atraccion atrac2 = new Atraccion("Parque",100,2,2,"Aventura");
+		
+		
+		atrac1.vender();
+		atrac2.vender();
+		
+		boolean real1 = atrac1.estaDisponible();
+		boolean real2 = atrac2.estaDisponible();
 		
 
-		ArrayList<Atraccion> atracciones = arc.leerAtracciones();
-		ArrayList<Promocion> promociones = arc.leerPromociones(atracciones);
+		Assert.assertEquals(esperado1,real1);
+		Assert.assertEquals(esperado2,real2);
 	}
 
 	@Test
-	public void DecrementoDeCupos() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
 	public void DecrementoDeDinero() {
-		fail("Not yet implemented");
+		/*
+		 * Se testea que al usuario se le reduzca el dinero en cada compra
+		 * */
+
+		double esperado = 54.2;
+		Atraccion atrac = new Atraccion("museo",45.8,4,1,"Paisaje");
+		Usuario usuario = new Usuario("jorge", 100, 5, "Aventura");
+
+		usuario.comprar(atrac);
+
+		double real = usuario.getDinero();
+
+		Assert.assertEquals(esperado, real, 0.01);
+
 	}
+
 	@Test
 	public void DecrementoDeTiempo() {
-		fail("Not yet implemented");
+		
+		/*
+		 * Se testea que al usuario se le reduzca el tiempo en cada compra
+		 * */
+		
+		double esperado = 1;
+		Atraccion atrac = new Atraccion("museo",45.8,4,1,"Paisaje");
+		Usuario usuario = new Usuario("jorge",100,5,"Aventura");
+		
+		usuario.comprar(atrac);
+		
+		double real = usuario.getTiempo();
+		
+		Assert.assertEquals(esperado, real,0.01);
 	}
+	
+	
 	@Test
-	public void NoOfrecerAtracDuplicadas() {
-		fail("Not yet implemented");
+	public void generacionItinirario() {
+		
+		/*
+		 * Se testea por cada compra se le1 agrege correctamente a las compras
+		 * del usuario
+		 * */
+		
+		ArrayList<Oferta> esperado = new ArrayList<Oferta>();
+		
+		Usuario usuario = new Usuario("jorge",1000,10,"Aventura");
+		
+		
+		Atraccion atrac1 = new Atraccion("museo",100,1,1,"Paisaje");
+		Atraccion atrac2 = new Atraccion("Parque",100,2,2,"Aventura");
+		Atraccion atrac3 = new Atraccion("Taberna de Moe", 45.8, 3, 10, "Aventura");
+		Atraccion atrac4 = new Atraccion("Planta de Energia Nuclear", 85.8, 2, 6, "Aventura");
+		ArrayList<Atraccion> atracPromo = new ArrayList<Atraccion>();
+		atracPromo.add(atrac3);
+		atracPromo.add(atrac4);
+		PromoAbs promo = new PromoAbs(atracPromo,10);
+		
+		
+		esperado.add(atrac1);
+		esperado.add(atrac2);
+		esperado.add(promo);
+		
+		usuario.comprar(atrac1);
+		usuario.comprar(atrac2);
+		usuario.comprar(promo);
+		
+		ArrayList<Oferta> real = usuario.getCompras();
+		
+		Assert.assertEquals(esperado, real);
 	}
-	@Test
-	public void NoOfrecerAtracSinTiempo() {
-		fail("Not yet implemented");
-	}
-	@Test
-	public void NoOfrecerAtracSinDinero() {
-		fail("Not yet implemented");
-	}
+
+
 
 }

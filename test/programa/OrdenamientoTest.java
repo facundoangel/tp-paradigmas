507ec10b24ef87fa132d9c0d8675b3becdf491be
facundoangel <facundonicolasangel@gmail.com>
2023-06-07 21:58:07 -0300

    terminado de test de ordenamiento

--- test/programa/OrdenamientoTest.java
+++ test/programa/OrdenamientoTest.java
@@ -2,6 +2,7 @@ package programa;
 
 import java.util.ArrayList;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -21,9 +22,22 @@ public class OrdenamientoTest {
 	private ArrayList<Oferta> prefDegustacionReal;
 	private ArrayList<Oferta> prefPaisajeReal;
 
-	@BeforeClass
+	private Atraccion plantaEnergia;
+	private Atraccion taberna;
+	private Atraccion casaArbol;
+	private Atraccion shelbyvile;
+	private Atraccion museo;
+	private Atraccion krustyBurguer;
+
+	Promocion promoAventura;
+	Promocion promoDegust;
+	Promocion promoPaisaje;
+
+	@Before
 	public void setUpBeforeClass() throws Exception {
-		Archivo arc = new Archivo("archivos-test/");
+
+		// ordeno arrays en los diferentes tipos de ordenamiento que el programa tiene
+		Archivo arc = new Archivo("archivos-test/ordenamiento/");
 
 		ArrayList<Atraccion> atracciones = arc.leerAtracciones();
 		ArrayList<Promocion> promociones = arc.leerPromociones(atracciones);
@@ -37,6 +51,34 @@ public class OrdenamientoTest {
 
 		Oferta.crearArraysPreferencias(ofertas, this.prefAventuraReal, this.prefDegustacionReal, this.prefPaisajeReal);
 
+		
+		
+		
+		
+		// creo las atracciones y promociones que se utilizaran en el tests
+		this.plantaEnergia = new Atraccion("Planta de Energia Nuclear", 85.8, 2, 6, "Aventura");
+		this.taberna = new Atraccion("Taberna de Moe", 45.8, 3, 10, "Aventura");
+		this.casaArbol = new Atraccion("Casa del Arbol", 52.3, 4, 4, "Paisaje");
+		this.shelbyvile = new Atraccion("Shelbyville", 80, 2, 5, "Paisaje");
+		this.museo = new Atraccion("Museo", 45.8, 2, 5, "Paisaje");
+		this.krustyBurguer = new Atraccion("Krusty Burger", 100, 1, 3, "Degustacion");
+
+		ArrayList<Atraccion> atraccionesPromo = new ArrayList<Atraccion>();
+
+		atraccionesPromo.add(plantaEnergia);
+		atraccionesPromo.add(taberna);
+		this.promoAventura = new PromoDesc(atraccionesPromo, 20);
+		atraccionesPromo.clear();
+
+		atraccionesPromo.add(krustyBurguer);
+		this.promoDegust = new PromoAbs(atraccionesPromo, 60);
+		atraccionesPromo.clear();
+
+		atraccionesPromo.add(museo);
+		atraccionesPromo.add(casaArbol);
+		atraccionesPromo.add(shelbyvile);
+		this.promoPaisaje = new PromoAxB(atraccionesPromo, 1);
+
 	}
 
 	@Test
@@ -47,9 +89,18 @@ public class OrdenamientoTest {
 		 * función de un usuario que prefiera las atraciones del tipo aventura
 		 */
 
-		ArrayList<Oferta> prefAventuraEsperado = new ArrayList<Oferta>();
+		ArrayList<Oferta> arrayEsperado = new ArrayList<Oferta>();
+		arrayEsperado.add(this.promoAventura);
+		arrayEsperado.add(this.plantaEnergia);
+		arrayEsperado.add(this.taberna);
+		arrayEsperado.add(this.promoPaisaje);
+		arrayEsperado.add(this.promoDegust);
+		arrayEsperado.add(this.krustyBurguer);
+		arrayEsperado.add(this.shelbyvile);
+		arrayEsperado.add(this.casaArbol);
+		arrayEsperado.add(this.museo);
 
-		boolean resultado = this.prefAventuraReal.equals(prefAventuraEsperado);
+		boolean resultado = this.prefAventuraReal.equals(arrayEsperado);
 
 		Assert.assertTrue(resultado);
 
@@ -63,9 +114,18 @@ public class OrdenamientoTest {
 		 * función de un usuario que prefiera las atraciones del tipo paisaje
 		 */
 
-		ArrayList<Oferta> prefAventuraEsperado = new ArrayList<Oferta>();
+		ArrayList<Oferta> arrayEsperado = new ArrayList<Oferta>();
+		arrayEsperado.add(this.promoPaisaje);
+		arrayEsperado.add(this.shelbyvile);
+		arrayEsperado.add(this.casaArbol);
+		arrayEsperado.add(this.museo);
+		arrayEsperado.add(this.promoAventura);
+		arrayEsperado.add(this.promoDegust);
+		arrayEsperado.add(this.krustyBurguer);
+		arrayEsperado.add(this.plantaEnergia);
+		arrayEsperado.add(this.taberna);
 
-		boolean resultado = this.prefAventuraReal.equals(prefAventuraEsperado);
+		boolean resultado = this.prefPaisajeReal.equals(arrayEsperado);
 
 		Assert.assertTrue(resultado);
 	}
@@ -78,9 +138,19 @@ public class OrdenamientoTest {
 		 * función de un usuario que prefiera las atraciones del tipo degustación
 		 */
 
-		ArrayList<Oferta> prefAventuraEsperado = new ArrayList<Oferta>();
+		ArrayList<Oferta> arrayEsperado = new ArrayList<Oferta>();
+		arrayEsperado.add(this.promoDegust);
+		arrayEsperado.add(this.krustyBurguer);
+		arrayEsperado.add(this.promoPaisaje);
+		arrayEsperado.add(this.promoAventura);
+		arrayEsperado.add(this.plantaEnergia);
+		arrayEsperado.add(this.shelbyvile);
+		arrayEsperado.add(this.casaArbol);
+		arrayEsperado.add(this.taberna);
+		arrayEsperado.add(this.museo);
 
-		boolean resultado = this.prefAventuraReal.equals(prefAventuraEsperado);
+
+		boolean resultado = this.prefDegustacionReal.equals(arrayEsperado);
 
 		Assert.assertTrue(resultado);
 	}
--- test/programa/VentaTest.java
+++ test/programa/VentaTest.java
@@ -2,17 +2,51 @@ package programa;
 
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
+import java.util.ArrayList;
+import org.junit.Before;
 import org.junit.Test;
 
 public class VentaTest {
+	/*
+	 * En este test se testea que ante cada accion de venta, afecte correctamente
+	 * a cada objeto que inteviene. Es decir a cada usuario le reste dinero y el tiempo
+	 * y a cada atraccion le reste el cupo correctamente
+	 * */
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	
+	
+	@Before
+	public void setUpBeforeClass() throws Exception {
+		Archivo arc = new Archivo("archivos-test/venta");
+		
+
+		ArrayList<Atraccion> atracciones = arc.leerAtracciones();
+		ArrayList<Promocion> promociones = arc.leerPromociones(atracciones);
 	}
 
 	@Test
-	public void test() {
+	public void DecrementoDeCupos() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void DecrementoDeDinero() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void DecrementoDeTiempo() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void NoOfrecerAtracDuplicadas() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void NoOfrecerAtracSinTiempo() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void NoOfrecerAtracSinDinero() {
 		fail("Not yet implemented");
 	}
 
